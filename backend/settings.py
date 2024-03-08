2c4eaf7f7ffc674fd20dc0736dae347977f89ada
sumindas <vrsumindas007@gmail.com>
2024-03-08 22:24:20 +0530

    celery

--- backend/settings.py
+++ backend/settings.py
@@ -244,6 +244,7 @@ CELERY_BROKER_URL= 'redis://127.0.0.1:6379'
 CELERY_ACCEPT_CONTENT= ['application/json']
 CELERY_RESULT_SERIALIZER= 'json'
 CELERY_TASK_SERIALIZER=  'json' 
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 CELERY_TIMEZONE= 'Asia/Kolkata' 
 CELERY_RESULT_BACKEND = 'django-db'
 
